20df670b4224f6f82a62da8d5b891b7881812901
Matheus Beuren <matheusbeuren@gmail.com>
2026-02-13 22:03:59 -0300

    Up CD and Dag

--- dags/spacex_etl_dag.py
+++ dags/spacex_etl_dag.py
@@ -1,8 +1,6 @@
 from airflow.decorators import dag, task
 from datetime import datetime
-import pandas as pd
-
-DB_CONN_STRING = "postgresql://airflow:airflow@postgres:5432/airflow"
+import os
 
 @dag(
     dag_id='spacex_etl_pipeline_v2',
@@ -18,16 +16,14 @@ def spacex_etl():
         import yaml
         import logging
         import sys
-        from include.src.extract.spacex_extractor import SpaceXExtractor
         
         sys.path.insert(0, '/usr/local/airflow/include')
+        from src.extract.spacex_extractor import SpaceXExtractor
 
         config_path = '/usr/local/airflow/include/config/config.yaml'
-
         with open(config_path, 'r') as f:
             config = yaml.safe_load(f)
         
-       
         logging.info(f"Dicionário de endpoints encontrado: {config['api'].get('endpoints')}")
         
         extractor = SpaceXExtractor(
@@ -36,7 +32,6 @@ def spacex_etl():
             timeout=config['api'].get('timeout', 30),
             retry_attempts=config['api'].get('retry_attempts', 3)
         )
-        
         
         endpoints = config['api'].get('endpoints', {})
         l_path = endpoints.get('launches', 'launches')
@@ -50,14 +45,11 @@ def spacex_etl():
     @task
     def transform_data(raw_data):
         import sys
-        from include.src.transform.spacex_transform import SpaceXTransformer
-        
         sys.path.insert(0, '/usr/local/airflow/include')
-        transformer = SpaceXTransformer()
-        launches_df = transformer.transform_launches(raw_data['launches'])
-        rockets_df = transformer.transform_rockets(raw_data['rockets'])
+        from src.transform.spacex_transform import SpaceXTransformer
         
-        final_df = transformer.transform_launches(raw_data['launches']) 
+        transformer = SpaceXTransformer()
+        final_df = transformer.transform_launches(raw_data['launches'])
         
         for col in final_df.select_dtypes(include=['datetime64', 'datetimetz']).columns:
             final_df[col] = final_df[col].dt.strftime('%Y-%m-%d %H:%M:%S')
@@ -66,18 +58,24 @@ def spacex_etl():
 
     @task
     def load_data(clean_data_dict):
-        from include.src.load.data_loader import DataLoader
         import pandas as pd
         import sys
+        
         sys.path.insert(0, '/usr/local/airflow/include')
+        from src.load.data_loader import DataLoader
         
         df = pd.DataFrame(clean_data_dict)
         loader = DataLoader()
         
+        connection_ = os.getenv(
+            'AIRFLOW__DATABASE__SQL_ALCHEMY_CONN',
+            'postgresql://postgres:postgres@postgres:5432/postgres'
+        )
+        
         loader.save_to_postgres(
             df=df,
             table_name='launches_analytics',
-            connection_string=DB_CONN_STRING
+            connection_string=connection_
         )
 
     raw = extract_data()
